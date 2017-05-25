39e01661c46767c7f5a23b0d0058bf9550734335
jslusser <jslusser@gmail.com>
2017-05-25 16:12:14 +0800

    Initial commit

---
+++ Project5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by James Slusser on 5/25/17.
+//  Copyright © 2017 James Slusser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
